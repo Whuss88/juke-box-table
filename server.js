43c22a6dcb360147d100d80026b627d4d394d751
Whuss88 <williamhussey95@gmail.com>
2024-11-19 13:50:06 -0500

    .http created and all routes tested

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const morgan = require("morgan")
 const prisma = require("./prisma");
 const app = express();
-const port = 3000;
+const PORT = 3000;
+
+app.use(morgan("dev"));
 
 app.use(express.json());
 
@@ -9,10 +12,14 @@ const userRoutes = require("./routes/users");
 const playlistRoutes = require("./routes/playlists");
 const trackRoutes = require("./routes/tracks");
 
+
 app.use("/users", userRoutes);
 app.use("/playlists", playlistRoutes);
 app.use("/tracks", trackRoutes);
 
+app.get("/", async (req,res,next) => {
+  console.log("Welcome to the Jukebox API.");
+});
 app.use((err,req,res,next) => {
   console.error(err);
   res.status(500).send("An error occured");
